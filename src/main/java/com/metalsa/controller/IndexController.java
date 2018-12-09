ed647f9f146b47cc5388cf1c0cb40ccb0f5bd665
Home <Home@192.168.2.7>
2018-12-09 23:43:02 +0530

    Added logging features

--- src/main/java/com/metalsa/controller/IndexController.java
+++ src/main/java/com/metalsa/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.metalsa.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class IndexController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping
     public String sayHello() {
+    	logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
         return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
     }
 }
