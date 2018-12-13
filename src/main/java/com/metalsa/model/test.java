efc40c76880af25410a9ebc9f248e31d4b6142cb
admin <admin@Pooja>
2018-12-13 22:55:18 +0530

    connection test class

---
+++ src/main/java/com/metalsa/model/test.java
@@ -0,0 +1,23 @@
+package com.metalsa.model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class test {
+	public static void main(String[] s) {
+		try{  
+			Class.forName("oracle.jdbc.driver.OracleDriver");  
+			Connection con=DriverManager.getConnection(  
+					"jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(HOST=130.61.80.158)\r\n" + 
+							"					(PORT=1521)(PROTOCOL=tcp))(CONNECT_DATA=\r\n" + 
+							"					(SID=etplnew)))","sys as sysdba","ETpl_2018#");  
+			System.out.println(con.isClosed());  
+			con.close();
+			System.out.println(con.isClosed());  
+
+		}catch(Exception e){ System.out.println(e);}  
+
+
+	}
+
+}
