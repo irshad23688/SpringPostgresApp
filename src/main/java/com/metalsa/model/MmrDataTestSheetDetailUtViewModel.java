ee2ed65081b1de605f19f7c31d9c21d1afb66eaf
Home <Home@192.168.2.8>
2019-01-21 03:09:32 +0530

    Added datasheet model

---
+++ src/main/java/com/metalsa/model/MmrDataTestSheetDetailUtViewModel.java
@@ -0,0 +1,22 @@
+package com.metalsa.model;
+
+import java.util.List;
+
+public class MmrDataTestSheetDetailUtViewModel {
+	
+	
+	private List<MmrHeaderAttributeMasterUtModel> headerAttributeDetail;
+
+	public List<MmrHeaderAttributeMasterUtModel> getHeaderAttributeDetail() {
+		return headerAttributeDetail;
+	}
+
+	public void setHeaderAttributeDetail(List<MmrHeaderAttributeMasterUtModel> headerAttributeDetail) {
+		this.headerAttributeDetail = headerAttributeDetail;
+	}
+
+	 
+	
+	
+	 
+}
